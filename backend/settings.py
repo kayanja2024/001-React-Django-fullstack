4bd886eaddd245b231b63604f5f9398f75148568
kayanja2024 <paulkayanja1989@gmail.com>
2025-04-10 17:20:22 +0300

    allowed host

--- backend/settings.py
+++ backend/settings.py
@@ -31,7 +31,7 @@ load_dotenv()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["zero01-react-django-fullstack.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
